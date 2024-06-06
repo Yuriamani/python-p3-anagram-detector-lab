97d69279c1677c9123c95c8fd8e6c37e514c7db4
Yuri <yuri.kamau@student.moringaschool.com>
2024-06-06 20:49:27 +0300

    anagram-detector-lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self,name):
+        self.name = name
+
+    def match(self, input_list):
+        anagrams = []
+        sorted_name = sorted(self.name)
+        
+        for word in input_list:
+            sorted_word = sorted(word)
+            if sorted_name == sorted_word:
+                anagrams.append(word)    
+        
+        return anagrams
